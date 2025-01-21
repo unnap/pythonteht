7eeece3efdeaf388f1ba6894cb38f738f0b5ca95
unnap <elina.postila@edu.keuda.fi>
2025-01-21 15:07:42 +0200

    muutin mod3 t4 vuosi-muuttujan floatista intiksi

--- moduuli3/tehtava4.py
+++ moduuli3/tehtava4.py
@@ -1,9 +1,9 @@
-vuosi = float(input('Anna vuosi: '))
-karkausvuosi = f'{vuosi:.0f} on karkausvuosi'
+vuosi = int(input('Anna vuosi: '))
+karkausvuosi = f'{vuosi} on karkausvuosi'
 
 if vuosi%4==0 and vuosi%100!=0:
     print('karkausvuosi')
 elif vuosi%400==0:
     print('karkausvuosi')
 else:
-    print(f'{vuosi:.0f} ei ole karkausvuosi')
+    print(f'{vuosi} ei ole karkausvuosi')
