a9f71d8b1fa982694aed48096dfa2c8e8dcda708
unnap <elina.postila@edu.keuda.fi>
2025-01-24 11:42:47 +0200

    korjausta

--- moduuli4/tehtava6.py
+++ moduuli4/tehtava6.py
@@ -2,13 +2,12 @@ import random
 
 pisteet = float(input('Syötä pisteiden määrä: '))
 N = n = 0
-r = 1
 
 while pisteet!=N:
     N+=1
-    x = random.uniform(0,1)
-    y = random.uniform(0,1)
-    if (x**2 + y**2) < r:
+    x = random.uniform(-1,1)
+    y = random.uniform(-1,1)
+    if (x**2 + y**2) < 1:
         n+=1
 
 print(f'Piin likiarvo on {4*n/N}')
